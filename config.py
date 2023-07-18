2043946b3d8d9e8bc068c691150c38df221499cb
mihalainen <doktorbashmak@gmail.com>
2023-07-18 20:32:48 +0300

    My 11 sprint

---
+++ config.py
@@ -0,0 +1,3 @@
+URL = "https://88aceffa-dafd-404a-a942-90c08d7973a5.serverhub.praktikum-services.ru" # адрес стенда
+CREATE_ORDER_API = "/api/v1/orders" # ручка создания заказа
+GET_ORDER_API = "/api/v1/orders/track" # ручка получения заказа по его трек-номеру
